943c5cbd52ea2fcc2295eaa4d59ce295b56899be
baidu <baidu@baidu.com>
2024-10-15 21:01:30 +0800

    更新 litecon.py

--- py/litecon.py
+++ py/litecon.py
@@ -1,38 +1,33 @@
-ip_version_priority = "ipv4"
+ip_version_priority = "ipv6"
 
 source_urls = [
-    "https://raw.githubusercontent.com/redrainl/iptv/main/speedtest/zubo_fofa.txt", #ADDED BY LEM ON 01/08/2024
-    "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
-    "https://raw.githubusercontent.com/pxiptv/live/main/iptv.txt", #ADDED BY LEM ON 08/08/2024
-    "http://tv.850930.xyz/kdsb.m3u", #ADDED BY LEM ON 29/07/2024
-    "http://tv.850930.xyz/kdsb2.m3u", #ADDED BY LEM ON 31/07/2024
-    "http://tv.850930.xyz/kdsb.txt", #ADDED BY LEM ON 28/09/2024
+    "https://live.zbds.top/tv/iptv6.txt", #ADDED BY LEM ON 15/10/2024
+    "https://live.fanmingming.com/tv/m3u/ipv6.m3u", #ADDED BY LEM ON 08/10/2024
+    "https://raw.githubusercontent.com/vbskycn/iptv/refs/heads/master/tv/iptv6.m3u", #ADDED BY LEM ON 15/10/2024
+    "http://ww.zouming.com/ipv6.txt", #ADDED BY LEM ON 31/07/2024
     "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
-    "https://gitcode.net/ygbh66/test/-/raw/master/oh.txt", #ADDED BY LEM ON 10/09/2024
-    "https://raw.githubusercontent.com/wwb521/live/main/tv.m3u", #ADDED BY LEM ON 28/09/2024
-    "https://raw.githubusercontent.com/kakaxi-1/IPTV/main/iptv.txt", #ADDED BY LEM ON 27/09/2024
-    "https://raw.githubusercontent.com/hus888yu/app/main/yt.txt", #ADDED BY LEM ON 06/10/2024    
-    "https://raw.githubusercontent.com/hus888yu/app/main/nb.txt", #ADDED BY LEM ON 27/09/2024
-    "https://raw.githubusercontent.com/hus888yu/app/main/ub.txt", #ADDED BY LEM ON 10/09/2024
-    "https://raw.githubusercontent.com/hus888yu/app/main/p3p.php", #ADDED BY LEM ON 10/09/2024
-    "https://www.stream-link.org/stream-link.m3u", #ADDED BY LEM ON 31/08/2024
-    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
-    "https://raw.githubusercontent.com/joevess/IPTV/main/iptv.m3u8", #ADDED BY LEM ON 08/09/2024  
-    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt", #ADDED BY LEM ON 29/07/2024
-    "http://ttkx.live:55/lib/kx2024.txt", #ADDED BY LEM ON 29/07/2024
-    "https://raw.githubusercontent.com/Love4vn/love4vn/main/Sport.m3u", #奥运 ON 29/07/2024
-    "https://gitlab.com/tvkj/qxitv/-/raw/main/888.txt" #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/wwb521/live/main/tv.m3u", #ADDED BY LEM ON 15/10/2024
+    "http://120.79.4.185/new/mdlive.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
+    "https://live.zhoujie218.top/tv/iptv6.txt",
+    "http://ww.weidonglong.com/dsj.txt",
+    "http://xhztv.top/zbc.txt",
+    "https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt",
+    "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
+    "https://v.nxog.top/m/tv/1/",
+    "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+    "http://home.jundie.top:81/Cat/tv/live.txt",
+    "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt", #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/refs/heads/main/live.txt" #ADDED BY LEM ON 29/07/2024
 ]
 
 url_blacklist = [
     "epg.pw/stream/",
     "103.40.13.71:12390",
-    "[2409:8087:1a01:df::4077]/PLTV/",
     "8.210.140.75:68",
     "154.12.50.54",
     "yinhe.live_hls.zte.com",
     "8.137.59.151",
-    "[2409:8087:7000:20:1000::22]:6060",
     "histar.zapi.us.kg",
     "www.tfiplaytv.vip",
     "dp.sxtv.top",
