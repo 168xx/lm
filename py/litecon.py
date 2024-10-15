961c509ac224366ad67b4480a5397aa50b3ff71e
baidu <baidu@baidu.com>
2024-10-15 20:08:11 +0800

    up

--- py/litecon.py
+++ py/litecon.py
@@ -1,8 +1,27 @@
-ip_version_priority = "ipv6"
+ip_version_priority = "ipv4"
 
 source_urls = [
-    "https://live.zbds.top/tv/iptv6.txt", #ADDED BY LEM ON 15/10/2024
-    "https://live.fanmingming.com/tv/m3u/ipv6.m3u", #ADDED BY LEM ON 08/10/2024
+    "https://raw.githubusercontent.com/redrainl/iptv/main/speedtest/zubo_fofa.txt", #ADDED BY LEM ON 01/08/2024
+    "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+    "https://raw.githubusercontent.com/pxiptv/live/main/iptv.txt", #ADDED BY LEM ON 08/08/2024
+    "http://tv.850930.xyz/kdsb.m3u", #ADDED BY LEM ON 29/07/2024
+    "http://tv.850930.xyz/kdsb2.m3u", #ADDED BY LEM ON 31/07/2024
+    "http://tv.850930.xyz/kdsb.txt", #ADDED BY LEM ON 28/09/2024
+    "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
+    "https://gitcode.net/ygbh66/test/-/raw/master/oh.txt", #ADDED BY LEM ON 10/09/2024
+    "https://raw.githubusercontent.com/wwb521/live/main/tv.m3u", #ADDED BY LEM ON 28/09/2024
+    "https://raw.githubusercontent.com/kakaxi-1/IPTV/main/iptv.txt", #ADDED BY LEM ON 27/09/2024
+    "https://raw.githubusercontent.com/hus888yu/app/main/yt.txt", #ADDED BY LEM ON 06/10/2024    
+    "https://raw.githubusercontent.com/hus888yu/app/main/nb.txt", #ADDED BY LEM ON 27/09/2024
+    "https://raw.githubusercontent.com/hus888yu/app/main/ub.txt", #ADDED BY LEM ON 10/09/2024
+    "https://raw.githubusercontent.com/hus888yu/app/main/p3p.php", #ADDED BY LEM ON 10/09/2024
+    "https://www.stream-link.org/stream-link.m3u", #ADDED BY LEM ON 31/08/2024
+    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
+    "https://raw.githubusercontent.com/joevess/IPTV/main/iptv.m3u8", #ADDED BY LEM ON 08/09/2024  
+    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt", #ADDED BY LEM ON 29/07/2024
+    "http://ttkx.live:55/lib/kx2024.txt", #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Love4vn/love4vn/main/Sport.m3u", #奥运 ON 29/07/2024
+    "https://gitlab.com/tvkj/qxitv/-/raw/main/888.txt" #ADDED BY LEM ON 29/07/2024
 ]
 
 url_blacklist = [
